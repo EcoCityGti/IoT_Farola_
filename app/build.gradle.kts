76f596e9c1c0cff1cc3fb7829f990852b0233884
Ruby García <rubgarqui@gmail.com>
2023-10-23 19:45:18 +0200

    facebook_v1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     // Import the BoM for the Firebase platform
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation ("com.facebook.android:facebook-android-sdk:[4,5)")
 
     // Add the dependency for the Firebase Authentication library
     // When using the BoM, you don't specify versions in Firebase library dependencies
