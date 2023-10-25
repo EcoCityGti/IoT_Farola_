d03c51f373c03069b77a15f6a531f5c1f4308581
Ruby García <rubgarqui@gmail.com>
2023-10-25 10:02:27 +0200

    Inicio_Correo_Personalizado_v1.5

--- app/src/main/java/com/example/iot_farola/LoginCorreo.java
+++ app/src/main/java/com/example/iot_farola/LoginCorreo.java
@@ -1,6 +1,9 @@
 package com.example.iot_farola;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +11,13 @@ public class LoginCorreo extends AppCompatActivity {
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_correo);
+        TextView Registro = findViewById(R.id.Registrarte);
+        Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Registro.class);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/iot_farola/Registro.java
@@ -0,0 +1,10 @@
+package com.example.iot_farola;
+
+import android.os.Bundle;
+
+public class Registro extends AppActivity{
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registro);
+    }
+}
