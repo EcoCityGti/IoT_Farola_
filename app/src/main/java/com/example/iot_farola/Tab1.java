6b8433be5eea7e20216a2d708c537515b1c445bc
Ruby García <rubgarqui@gmail.com>
2023-10-28 20:32:17 +0200

    Cambios_tab1_v1.2

--- app/src/main/java/com/example/iot_farola/Tab1.java
+++ app/src/main/java/com/example/iot_farola/Tab1.java
@@ -1,10 +1,17 @@
 package com.example.iot_farola;
 
+import static android.opengl.ETC1.getHeight;
 import static com.firebase.ui.auth.AuthUI.getApplicationContext;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -92,8 +99,36 @@ public class Tab1 extends Fragment {
                         cache.put(url, bitmap);
                     }
                     public Bitmap getBitmap(String url) {
-                        return cache.get(url);
+                        Bitmap output = cache.get(url);
+
+                        if (output == null) {
+                            // La imagen no está en la caché, no podemos aplicar el recorte circular.
+                            return null;
+                        }
+
+                        int width = output.getWidth();
+                        int height = output.getHeight();
+                        int diameter = Math.min(width, height);
+                        Bitmap circularBitmap = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888);
+
+                        Canvas canvas = new Canvas(circularBitmap);
+                        final int color = 0xff424242;
+                        final Paint paint = new Paint();
+                        final Rect rect = new Rect(0, 0, diameter, diameter);
+                        final RectF rectF = new RectF(rect);
+                        final float roundPx = diameter / 2;
+
+                        paint.setAntiAlias(true);
+                        canvas.drawARGB(0, 0, 0, 0);
+                        paint.setColor(color);
+                        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+                        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+                        canvas.drawBitmap(output, rect, rect, paint);
+
+                        return circularBitmap;
                     }
+
                 });
 // Foto de usuario
         Uri urlImagen = usuario.getPhotoUrl();
