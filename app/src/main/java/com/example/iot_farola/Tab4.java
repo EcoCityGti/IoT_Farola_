8f435f8f03c1e2e429e25c3662a898cd44696055
Ruby García <rubgarqui@gmail.com>
2023-10-29 00:00:04 +0200

    Cambios_aspecto_v1.1

--- app/src/main/java/com/example/iot_farola/Tab4.java
+++ app/src/main/java/com/example/iot_farola/Tab4.java
@@ -4,6 +4,12 @@ import static com.firebase.ui.auth.AuthUI.getApplicationContext;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.LruCache;
@@ -37,10 +43,53 @@ public class Tab4 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.edicion_cuenta, container, false);
-        //super.onCreate(savedInstanceState);
-        //Toolbar toolbar = (Toolbar) v.findViewById(R.id.toolbar);
-
         FirebaseUser usuario = FirebaseAuth.getInstance().getCurrentUser();
+        RequestQueue colaPeticiones = Volley.newRequestQueue(requireActivity());
+        ImageLoader lectorImagenes = new ImageLoader(colaPeticiones,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap> cache =
+                            new LruCache<String, Bitmap>(10);
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                    public Bitmap getBitmap(String url) {
+                        Bitmap output = cache.get(url);
+
+                        if (output == null) {
+                            // La imagen no está en la caché, no podemos aplicar el recorte circular.
+                            return null;
+                        }
+
+                        int width = output.getWidth();
+                        int height = output.getHeight();
+                        int diameter = Math.min(width, height);
+                        Bitmap circularBitmap = Bitmap.createBitmap(diameter, diameter, Bitmap.Config.ARGB_8888);
+
+                        Canvas canvas = new Canvas(circularBitmap);
+                        final int color = 0xff424242;
+                        final Paint paint = new Paint();
+                        final Rect rect = new Rect(0, 0, diameter, diameter);
+                        final RectF rectF = new RectF(rect);
+                        final float roundPx = diameter / 2;
+
+                        paint.setAntiAlias(true);
+                        canvas.drawARGB(0, 0, 0, 0);
+                        paint.setColor(color);
+                        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+                        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+                        canvas.drawBitmap(output, rect, rect, paint);
+
+                        return circularBitmap;
+                    }
+                });
+        Uri urlImagen = usuario.getPhotoUrl();
+        if (urlImagen != null) {
+            NetworkImageView foto = (NetworkImageView) v.findViewById(R.id.imagen5);
+            foto.setImageUrl(urlImagen.toString(), lectorImagenes);
+        }
+
+
         btnAnonimo = v.findViewById(R.id.UnificarCuenta);
         EditText nombre = v.findViewById(R.id.nombre);
         nombre.setText(usuario.getDisplayName());
@@ -113,25 +162,6 @@ public class Tab4 extends Fragment {
             }
         });
 
-
-        RequestQueue colaPeticiones = Volley.newRequestQueue(requireActivity());
-        ImageLoader lectorImagenes = new ImageLoader(colaPeticiones,
-                new ImageLoader.ImageCache() {
-                    private final LruCache<String, Bitmap> cache =
-                            new LruCache<String, Bitmap>(10);
-                    public void putBitmap(String url, Bitmap bitmap) {
-                        cache.put(url, bitmap);
-                    }
-                    public Bitmap getBitmap(String url) {
-                        return cache.get(url);
-                    }
-                });
-// Foto de usuario
-        /*Uri urlImagen = usuario.getPhotoUrl();
-        if (urlImagen != null) {
-            NetworkImageView foto = (NetworkImageView) v.findViewById(R.id.imagen);
-            foto.setImageUrl(urlImagen.toString(), lectorImagenes);
-        }*/
         if (usuario != null && usuario.isAnonymous()) {
             // El usuario actual es anónimo, por lo que muestras el botón
             button.setVisibility(View.GONE);
